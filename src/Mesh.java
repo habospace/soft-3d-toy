d2b220c385642863092010e2b99e79ee45cef4a9
habospace <avatarokwow@gmail.com>
2016-03-30 21:37:24 +0100

    update: add line features to Mesh

--- src/Mesh.java
+++ src/Mesh.java
@@ -5,18 +5,21 @@ public class Mesh {
 
     private int verticescount;
     private Vector[] vertices;
-    private int[][] projection;
+    private int maxlinescount;
+    private int[][] lines;
 
     public Mesh(int verticescount){
         this.verticescount = verticescount;
         this.vertices = new Vector [verticescount];
-        this.projection = new int[verticescount][2];
+        this.maxlinescount = getMaximumLines(verticescount);
+        this.lines = new int[maxlinescount][2];
     }
 
     public Mesh(){
         this.verticescount = 8;
         this.vertices = new Vector[verticescount];
-        this.projection = new int[verticescount][2];
+        this.maxlinescount = getMaximumLines(verticescount);
+        this.lines = new int[maxlinescount][2];
         AddVertex(new Vector (5, -5, 50), 0);
         AddVertex(new Vector (-5, -5, 50), 1);
         AddVertex(new Vector (-5, 5, 50), 2);
@@ -27,12 +30,26 @@ public class Mesh {
         AddVertex(new Vector (5, 5, 60), 7);
     }
 
+    private int getMaximumLines(int verticescount){
+        return (verticescount*(verticescount -1))/2;
+    }
+
     public void AddVertex(Vector vertex, int index){
         try {
             vertices[index] = vertex;
         }
         catch (ArrayIndexOutOfBoundsException error){
             System.out.println("Exception thrown  :" + error);
+        }
+    }
+
+    public void addLine(int vertex1, int vertex2, int index){
+        try{
+            lines[index][0] = vertex1;
+            lines[index][1] = vertex2;
+        }
+        catch (ArrayIndexOutOfBoundsException error){
+            System.out.println("Exception thrown :" + error);
         }
     }
 
